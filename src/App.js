93f087a7c6d9b7faaf8937743806b284e348a1ac
sandynagara <tausan22@gmail.com>
2022-10-14 01:02:23 +0700

    fix bug in deployment

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import Dashboard from "./pages/Dashboard";
-import {Routes, Route, BrowserRouter } from "react-router-dom";
+import {Routes, Route, HashRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<LandingPage />}/>
           <Route path="/dashboard" element={<Dashboard />}/>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
