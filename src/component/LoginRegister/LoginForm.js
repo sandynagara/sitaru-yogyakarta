ccb40d98a0d4f601c6ef0eef7d34dfd1f3489dc0
sandynagara <tausan22@gmail.com>
2023-03-30 14:40:04 +0700

    refactor: Frontend refactor

--- src/component/LoginRegister/LoginForm.js
+++ src/component/LoginRegister/LoginForm.js
@@ -2,9 +2,10 @@ import React from 'react'
 import  {AiOutlineUser ,AiOutlineLock,AiOutlineClose} from 'react-icons/ai'
 import configData from "../config.json"
 import Swal from 'sweetalert2'
+import Modal from '@mui/material/Modal';
 import InputLoginRegister from './InputLoginRegister'
 
-function LoginForm({setLogin,setOpen}){
+function LoginForm({setLogin,setOpen,open}){
     var submitHandler = (event) =>{
         event.preventDefault();
         var username = event.target.Username.value
@@ -27,6 +28,7 @@ function LoginForm({setLogin,setOpen}){
             })
             .then(res=>res.json())
             .then(hasil=>{
+                console.log();
                 if(hasil.RTN){
                     setOpen(false)
                     setLogin(true)
@@ -48,21 +50,27 @@ function LoginForm({setLogin,setOpen}){
     }
 
     return (
-        <div className="login-container">
-            <div style={{width:"100%",display:"flex",padding:0,minWidth:"100%",justifyContent:"justify-between",alignItems:"center"}}>
-                <h2 style={{width:"100%"}} className="font-bold">Login</h2>
-                <AiOutlineClose style={{cursor:"pointer"}} size={22} color="red" onClick={()=>setOpen(false)}/>
-            </div>
-            <form onSubmit={(e)=> submitHandler(e)}>
-                <InputLoginRegister tipe="text" label="Username" logo={<AiOutlineUser/>}/>
-                <InputLoginRegister tipe="password" label="Password" logo={<AiOutlineLock/>}/>
-                <button type="submit" className='p-2'>Login </button>
-            </form>
-            {/* <div className='register-link'>
-                <p>Belum punya akun?</p>
-                <p className='to-register' onClick={()=>setLogin(false)}>Register</p>
-            </div> */}
-        </div>
+        <Modal open={open}
+            onClose={()=>setOpen(!open)}
+        >
+            <div className='fixed bg-black bg-opacity-60 top-0 w-screen h-screen items-center flex justify-center'>
+                <div className="login-container">
+                    <div style={{width:"100%",display:"flex",padding:0,minWidth:"100%",justifyContent:"justify-between",alignItems:"center"}}>
+                        <h2 style={{width:"100%"}} className="font-bold">Login</h2>
+                        <AiOutlineClose style={{cursor:"pointer"}} size={22} color="red" onClick={()=>setOpen(false)}/>
+                    </div>
+                    <form onSubmit={(e)=> submitHandler(e)}>
+                        <InputLoginRegister tipe="text" label="Username" logo={<AiOutlineUser/>}/>
+                        <InputLoginRegister tipe="password" label="Password" logo={<AiOutlineLock/>}/>
+                        <button type="submit" className='p-2'>Login </button>
+                    </form>
+                    {/* <div className='register-link'>
+                        <p>Belum punya akun?</p>
+                        <p className='to-register' onClick={()=>setLogin(false)}>Register</p>
+                    </div> */}
+                </div>
+            </div> 
+        </Modal>
     )
 } 
 
